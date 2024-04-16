f319c947f4687686c333680b7e368ed2722cf15c
Moacyr kennedy <kennedymoacyr@gmail.com>
2024-04-16 18:16:38 -0300

    Convencoes_boas_praticas

---
+++ variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = "guilherme"
+
+idade = 28
+
+nome = "Giovanna"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+
+BRAZILIAN_STATES = ["sp", "RJ", "SC", "RS"]
+
+
+print(BRAZILIAN_STATES)
